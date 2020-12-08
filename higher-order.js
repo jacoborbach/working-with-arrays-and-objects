cb15271176adbc7d3caa770249aac3608f747425
jacoborbach <jacoborbach96@gmail.com>
2020-12-07 20:01:20 -0500

    latest

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+// let evenNumbers  = mixedNumbers.filter(function(element, index, array) {
+//   return element % 2 == 0;
+// })
+
+let evenNumbers = mixedNumbers.filter(element => element % 2 == 0)
 
 
 
@@ -44,7 +48,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+// let postTaxPrices = prices.map(function (element, index, array) {
+//   return element *= 1.07
+// });
+
+let postTaxPrices = prices.map (element => element *= 1.07)
 
 
 
@@ -63,7 +71,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+// let totalPopulation  = populations.reduce(function(acc, curr) {
+//   return acc + curr;
+// })
+
+let totalPopulation = populations.reduce((acc, curr) => acc + curr);
 
 
 
